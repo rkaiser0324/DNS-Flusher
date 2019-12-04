349998605c4138c4f963d644de2a61f4822071f3
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2019-12-04 10:07:06 -0800

    fix: prevent multiple overlays from being instantiated, and increase z-index so it it more likely to show up
    https://app.asana.com/0/37213698877677/1151237493508959

--- content.js
+++ content.js
@@ -1,71 +1,77 @@
 var updateDnsFlusherStatusUI = function(pageJson) {
   var page = JSON.parse(pageJson);
 
-  var div = document.createElement("div");
-  div.id = "dns_flusher_page_status";
+  var element = document.getElementById("dns_flusher_page_status");
+  if (!(typeof element != "undefined" && element != null)) {
+    var div = document.createElement("div");
+    div.id = "dns_flusher_page_status";
 
-  var div_left = document.createElement("div");
-  div_left.className = "left";
-  div_left.innerHTML = '<div> &harr; </div>';
+    var div_left = document.createElement("div");
+    div_left.className = "left";
+    div_left.innerHTML = "<div> &harr; </div>";
 
-  var div_right = document.createElement("div");
-  div_right.className = "right";
+    var div_right = document.createElement("div");
+    div_right.className = "right";
 
-  var div_ip = document.createElement("div");
-  div_ip.className = "ip";
-  div_instance = document.createElement("div");
-  div_instance.className = "instance";
-  div_php_version = document.createElement("div");
-  div_php_version.className = "php_version";
-  div_proxy_cache = document.createElement("div");
-  div_proxy_cache.className = "proxy_cache";
+    var div_ip = document.createElement("div");
+    div_ip.className = "ip";
+    div_instance = document.createElement("div");
+    div_instance.className = "instance";
+    div_php_version = document.createElement("div");
+    div_php_version.className = "php_version";
+    div_proxy_cache = document.createElement("div");
+    div_proxy_cache.className = "proxy_cache";
 
-  div_right.appendChild(div_ip);
-  div_right.appendChild(div_instance);
-  div_right.appendChild(div_php_version);
-  div_right.appendChild(div_proxy_cache);
-  div.appendChild(div_left);
-  div.appendChild(div_right);
-  document.body.appendChild(div);
+    div_right.appendChild(div_ip);
+    div_right.appendChild(div_instance);
+    div_right.appendChild(div_php_version);
+    div_right.appendChild(div_proxy_cache);
+    div.appendChild(div_left);
+    div.appendChild(div_right);
+    document.body.appendChild(div);
 
-  document.querySelector("#dns_flusher_page_status .ip").innerText = page.ip;
-  document.querySelector("#dns_flusher_page_status .instance").innerText =
-    page.instanceName;
-  document.querySelector("#dns_flusher_page_status .php_version").innerText =
-    page.phpVersion == "" ? "" : "PHP " + page.phpVersion;
-  document.querySelector("#dns_flusher_page_status .proxy_cache").innerText = page.proxyCache == "" ? "" : "Cache " + page.proxyCache;
+    document.querySelector("#dns_flusher_page_status .ip").innerText = page.ip;
+    document.querySelector("#dns_flusher_page_status .instance").innerText =
+      page.instanceName;
+    document.querySelector("#dns_flusher_page_status .php_version").innerText =
+      page.phpVersion == "" ? "" : "PHP " + page.phpVersion;
+    document.querySelector("#dns_flusher_page_status .proxy_cache").innerText =
+      page.proxyCache == "" ? "" : "Cache " + page.proxyCache;
 
-  div.addEventListener(
-    "click",
-    function(event) {
-      event.preventDefault();
+    div.addEventListener(
+      "click",
+      function(event) {
+        event.preventDefault();
 
-      var d = document.getElementById("dns_flusher_page_status");
-      if (hasClass(d, "offscreen")) {
-        d.style.right = "5px";
-        removeClass(d, "offscreen");
-      } else {
-        var buttonWidth = document.querySelector("#dns_flusher_page_status .left").offsetWidth;
-        d.style.right = -(d.offsetWidth - buttonWidth - 10) + "px";
-        addClass(d, "offscreen");
+        var d = document.getElementById("dns_flusher_page_status");
+        if (hasClass(d, "offscreen")) {
+          d.style.right = "5px";
+          removeClass(d, "offscreen");
+        } else {
+          var buttonWidth = document.querySelector(
+            "#dns_flusher_page_status .left"
+          ).offsetWidth;
+          d.style.right = -(d.offsetWidth - buttonWidth - 10) + "px";
+          addClass(d, "offscreen");
+        }
+      },
+      false
+    );
+
+    // Helper functions from https://stackoverflow.com/a/28344281 and http://jaketrent.com/post/addremove-classes-raw-javascript/
+    function hasClass(ele, cls) {
+      return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+    }
+
+    function addClass(ele, cls) {
+      if (!hasClass(ele, cls)) ele.className += " " + cls;
+    }
+
+    function removeClass(ele, cls) {
+      if (hasClass(ele, cls)) {
+        var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+        ele.className = ele.className.replace(reg, " ");
       }
-    },
-    false
-  );
-
-  // Helper functions from https://stackoverflow.com/a/28344281 and http://jaketrent.com/post/addremove-classes-raw-javascript/
-  function hasClass(ele, cls) {
-    return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
-  }
-
-  function addClass(ele, cls) {
-    if (!hasClass(ele, cls)) ele.className += " " + cls;
-  }
-
-  function removeClass(ele, cls) {
-    if (hasClass(ele, cls)) {
-      var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-      ele.className = ele.className.replace(reg, " ");
     }
   }
 };
